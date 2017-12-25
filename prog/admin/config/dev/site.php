98492903120b2c49e91b5dd17329be1eda6876db
banxuelai160714 <banxuelai>
2017-12-25 17:36:29 +0800

    site

--- prog/admin/config/dev/site.php
+++ prog/admin/config/dev/site.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'title' => '智博教育成考系统',
+    'title' => '智博继续教育',
     'static_base' => 'http://47.104.93.177/static',
     'base_url' => '//'.$_SERVER['HTTP_HOST'],
     'log_dir' => '/tmp/happycode/old',
