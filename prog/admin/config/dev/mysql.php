d7306051b73b87f15b7e009b44cc8c8a0eeefda4
banxuelai160714 <banxuelai>
2017-12-26 11:31:08 +0800

    fix

--- prog/admin/config/dev/mysql.php
+++ prog/admin/config/dev/mysql.php
@@ -1,45 +1,26 @@
 <?php
 /**
- * @author banxuelai@vread.cn
- * @date 2017-11-14
+ * @author bxl@gmail.com
+ * @date 2017-12-26
  * 数据库配置文件
  */
 return array(
     'default' => array(
         'master' => array(
-            'host' => 'localhost',
+            'host' => '127.0.0.1',
             'user' => 'root',
-            'password' => '20170711.HS.test',
-            'dbname' => 'happycode_old',
+            'password' => 'B5aRAFw2Bine96Eh',
+            'dbname' => 'aducode',
             'charset' => 'utf8mb4', //支持emoji表情
             'port' => 3306,
         ),
         'slave' => array(
-            'host' => 'localhost',
+            'host' => '127.0.0.1',
             'user' => 'root',
-            'password' => '20170711.HS.test',
-            'dbname' => 'happycode_old',
+            'password' => 'B5aRAFw2Bine96Eh',
+            'dbname' => 'aducode',
             'charset' => 'utf8mb4', //支持emoji表情
             'port' => 3306,
         ),
-    ),
-    //新数据
-    'new' => array(
-            'master' => array(
-                    'host' => 'localhost',
-                    'user' => 'root',
-                    'password' => '20170711.HS.test',
-                    'dbname' => 'happycode',
-                    'charset' => 'utf8mb4', //支持emoji表情
-                    'port' => 3306,
-            ),
-            'slave' => array(
-                    'host' => 'localhost',
-                    'user' => 'root',
-                    'password' => '20170711.HS.test',
-                    'dbname' => 'happycode',
-                    'charset' => 'utf8mb4', //支持emoji表情
-                    'port' => 3306,
-            ),
     ),
 );
