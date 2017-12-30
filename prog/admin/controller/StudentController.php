1f8a8b16053b0dbb1651de4d43b48dd4df6e232c
banxuelai160714 <banxuelai>
2017-12-30 20:14:14 +0800

    fixed

--- prog/admin/controller/StudentController.php
+++ prog/admin/controller/StudentController.php
@@ -21,6 +21,18 @@ class StudentController extends AuthController
     //添加
     public function add()
     {
+        $student_model = new StudentModel();
+        $agent_model = new AgentModel();
+        $operation_model = new OperationModel();
+        $confirm_model = new ConfirmModel();
+        
+        if ($this->req->method == 'POST') {
+        }
+        
+        //二级代理
+        //确认点
+        //报考层次
+        
         $this->display('student/add.html', array(
                 'title' => '录入信息',
                 'nickname' => $this->getUserName(),
