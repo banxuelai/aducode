103d3129c0977dcafb2f2ac17c38276e0ddb88bb
banxuelai160714 <banxuelai>
2018-01-26 21:25:28 +0800

    fixed

--- prog/admin/config/site.php
+++ prog/admin/config/site.php
@@ -3,5 +3,5 @@ return array(
     'title' => '智博继续教育',
     'static_base' => 'http://47.104.90.191/static',
     'base_url' => '//'.$_SERVER['HTTP_HOST'],
-    'log_dir' => '/data/apps/var/logs/aducode/admin',
+    'log_dir' => '/tmp/data/aducode',
 );
