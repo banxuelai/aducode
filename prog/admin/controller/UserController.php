4a317cfb02fd24a1f1c991f4d3bcdd65ecfb1988
banxuelai160714 <banxuelai>
2018-01-26 22:21:57 +0800

    fex

--- prog/admin/controller/UserController.php
+++ prog/admin/controller/UserController.php
@@ -25,7 +25,7 @@ class UserController extends AuthController
              
             //check 重复
             $info = $user_model->getRow(array('nickname' => $nickname));
-            if (!$info) {
+            if ($info) {
                 $nickname .= '1';
             }
             $data = array(
